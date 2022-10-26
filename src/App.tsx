8eddc98885fe95f89fc61d30f931f9efb5509a9a
N2-Met <yuyushiki11@gmail.com>
2022-10-26 23:20:38 +0900

    "update"

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { useEffect, useState } from "react";
 import { ListItem } from "./components/ListItem";
 import axios from "axios";
 
+type User = {
+  id: number;
+  name: string;
+  age: number;
+  personalColor: string;
+}
+
 export const App = () => {
   //取得したユーザ情報
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   //画面表示時にユーザ情報取得
   useEffect(() => {
-    axios.get("https://example.com/users"), then((res) => {
+    // axios.get("https://example.com/users"). then((res) => {
+      axios.get<User[]>("https://example.com/users"). then((res) => {
       setUsers(res.data);
     })
   }, []);
